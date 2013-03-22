a43aabb0342be6c4bcc9c046af89eeed2ca5967f
ed tsang <edwardt.mailmail.com>
2013-03-22 10:40:39 -0700

    adding function comments but need tailor further of which won't use

--- ejabberd_auth_spark/src/ejabberd_auth_spark.erl
+++ ejabberd_auth_spark/src/ejabberd_auth_spark.erl
@@ -59,6 +59,9 @@ start(_Host) ->
     ?DEBUG("Spark authentication with status: ~p~n", [RETVAL]),    
     RETVAL.
 
+%% @spec (User::string(), Server::string(), Password::string()) ->
+%%       ok | {error, ErrorType}
+%% where ErrorType = empty_password | not_allowed | invalid_jid
 set_password(_User, _Server, _Password) ->
     %% TODO security issue to log this, doit another way but also enough info for debugging
     ?DEBUG("~p with user ~p server ~p password ~p~n", [?CURRENT_FUNCTION_NAME(),_User, _Server, _Password]),
@@ -66,6 +69,10 @@ set_password(_User, _Server, _Password) ->
     ?DEBUG("~p with status ~p~n", [?CURRENT_FUNCTION_NAME(), RETVAL]),
     RETVAL.
 
+%% @doc Check if the user and password can login in server.
+%% @spec (User::string(), Server::string(), Password::string(),
+%%        Digest::string(), DigestGen::function()) ->
+%%     true | false
 check_password(User, Server, Password, _Digest, _DigestGen) ->
     ?DEBUG("~p with user ~p server ~p password ~p digest ~p digestgen ~p~n", 
 	   [?CURRENT_FUNCTION_NAME(), User, Server, Password, _Digest, _DigestGen]),
@@ -73,6 +80,9 @@ check_password(User, Server, Password, _Digest, _DigestGen) ->
     ?DEBUG("~p with status ~p~n", [?CURRENT_FUNCTION_NAME(), RETVAL]),
     RETVAL.
 
+%% @doc Check if the user and password can login in server.
+%% @spec (User::string(), Server::string(), Password::string()) ->
+%%     true | false
 check_password(User, Host, Password) ->
     ?DEBUG("~p with user ~p host ~p password ~p~n", [?CURRENT_FUNCTION_NAME(), User, Host, Password]),
     
@@ -80,24 +90,29 @@ check_password(User, Host, Password) ->
     ?DEBUG("~p with status ~p~n", [?CURRENT_FUNCTION_NAME(), RETVAL]),
     RETVAL.
 
+%% @spec (User, Server, Password) -> {atomic, ok} | {atomic, exists} | {error, not_allowed}
 try_register(_User, _Server, _Password) ->
     ?DEBUG("~p with user ~p server ~p password ~p~n", [?CURRENT_FUNCTION_NAME(), _User, _Server, _Password]),
     RETVAL = {error, not_allowed},
     ?DEBUG("~p with status ~p~n", [?CURRENT_FUNCTION_NAME(), RETVAL]),
     RETVAL.
 
+%% @doc Registered users list do not include anonymous users logged
 dirty_get_registered_users() ->
     ?DEBUG("~p~n", [?CURRENT_FUNCTION_NAME()]),
     RETVAL = [],
     ?DEBUG("~p with status ~p~n", [?CURRENT_FUNCTION_NAME(), RETVAL]),
     RETVAL.
 
+%% @doc Registered users list do not include anonymous users logged
 get_vh_registered_users(_Host) ->
     ?DEBUG("~p with host ~p~n", [?CURRENT_FUNCTION_NAME(), _Host]),
     RETVAL = [],
     ?DEBUG("~p with status ~p~n", [?CURRENT_FUNCTION_NAME(), RETVAL]),
     RETVAL.
 
+%% @doc Get the password of the user.
+%% @spec (User::string(), Server::string()) -> Password::string()
 get_password(_User, _Server) ->
     ?DEBUG("~p with user ~p server ~p password ~p~n", [?CURRENT_FUNCTION_NAME(), _User, _Server]),
     RETVAL = false,
@@ -115,16 +130,26 @@ get_password_s(_User, _Server) ->
 is_user_exists(User, Host) ->
     {error, not_implemented}.
 
+%% @spec (User, Server) -> ok
+%% @doc Remove user.
+%% Note: it may return ok even if there was some problem removing the user.
 remove_user(_User, _Server) ->
     {error, not_allowed}.
 
+%% @spec (User, Server, Password) -> ok | not_exists | not_allowed | bad_request | error
+%% @doc Try to remove user if the provided password is correct.
+%% The removal is attempted in each auth method provided:
+%% when one returns 'ok' the loop stops;
+%% if no method returns 'ok' then it returns the error message indicated by the last method attempted.
 remove_user(_User, _Server, _Password) ->
     not_allowed.
 
+%% @doc This is only executed by ejabberd_c2s for non-SASL auth client
 plain_password_required() ->
     
     true.
 
+%% @doc Flag to indicate if using external storage to cache credentials
 store_type() ->
     external.
 
@@ -137,6 +162,10 @@ store_type() ->
 
 %%%%%% EUNIT %%%%%%%%%%%%%%%%%%
 -ifdef(TEST).
+
+
+
+
 
 
 -endif.
