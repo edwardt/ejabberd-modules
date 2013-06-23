288730b7d6c436edfc773d5a0212d5acfcd2095f
root <root@chat.dev0.spark.net>
2013-06-23 15:47:54 -0400

    wired up amqp

--- mod_spark_log_chat/build.sh
+++ mod_spark_log_chat/build.sh
@@ -2,5 +2,6 @@
 EJABBERD_DEV=../ejabberd-dev/trunk
 AMQP_DEV=../amqp_client
 RABBITC_DEV=../rabbit_common
+SPARK_APP_CONFIG=./apps/spark_common_util
 
-erl -pa ${EJABBERD_DEV}/ebin ${AMQP_DEV}/ebin ${RABBITC_DEV}/ebin -pz ebin -make
+erl -pa ${EJABBERD_DEV}/ebin ${SPARK_APP_CONFIG}/ebin ${AMQP_DEV}/ebin ${RABBITC_DEV}/ebin -pz ebin -make
