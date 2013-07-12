8b5ff78f9d1aacbf7a594bbbceb3b2611d65de77
ed <dontemailme@dontemailme.com>
2013-07-12 14:54:10 -0700

    dup definintion

--- mod_spark_log_chat/src/chat_message_model.erl
+++ mod_spark_log_chat/src/chat_message_model.erl
@@ -4,19 +4,6 @@
 -behaviour(json_rec_model).
 
 -export([new/1]).
-
--record(chat_message, {
-		from = "" ::string(),
-		from_brandId = "" ::string(),
-		to = "" ::string(),
-		to_brandId = "" ::string(),
-		type = "chat" ::string(), 
-		format = "text" ::string(),
-		subject = "" ::string(), 
-		body = "" ::string(), 
-		thread = "" ::string(),
-		time_stamp = "" ::string()}).
-
 new(<<"chat_message">>)->
    '#chat_message'();
 new(_)-> undefined.
