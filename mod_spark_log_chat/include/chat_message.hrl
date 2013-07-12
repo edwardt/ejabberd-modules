57eef8c0d822e5cd7adce55aad85d8235a586be3
root <root@chat.dev0.spark.net>
2013-07-12 18:48:41 -0400

    fixed the undef problem

--- mod_spark_log_chat/include/chat_message.hrl
+++ mod_spark_log_chat/include/chat_message.hrl
@@ -1,14 +1,14 @@
 -record(chat_message, {
-		from = "" ::string(),
-		from_brandId = "" ::string(),
-		to = "" ::string(),
-		to_brandId = "" ::string(),
-		type = "chat" ::string(), 
-		format = "text" ::string(),
-		subject = "" ::string(), 
-		body = "" ::string(), 
-		thread = "" ::string(),
-		time_stamp = "" ::string()}).
+		from ::string(),
+		from_brandId  ::string(),
+		to  ::string(),
+		to_brandId ::string(),
+		type ::string(), 
+		format ::string(),
+		subject ::string(), 
+		body ::string(), 
+		thread ::string(),
+		time_stamp ::string()}).
 
 
 -export_records([chat_message]).
--- mod_spark_log_chat/src/chat_message_model.erl
+++ mod_spark_log_chat/src/chat_message_model.erl
@@ -1,26 +1,14 @@
 -module(chat_message_model).
-%-include_lib("chat_message.hrl").
-
+-include_lib("chat_message.hrl").
 -behaviour(json_rec_model).
--compile([parse_transform, exprecs]).
--export([new/1]).
 
--record(chat_message, {
-		from = "" ::string(),
-		from_brandId = "" ::string(),
-		to = "" ::string(),
-		to_brandId = "" ::string(),
-		type = "chat" ::string(), 
-		format = "text" ::string(),
-		subject = "" ::string(), 
-		body = "" ::string(), 
-		thread = "" ::string(),
-		time_stamp = "" ::string()}).
-
-
--export_records([chat_message]).
-
+-compile({parse_transform, exprecs}).
+-export([new/1, 
+	 rec/1]).
 
 new(<<"chat_message">>)->
    '#new-chat_message'();
 new(_)-> undefined.
+
+rec(#chat_message{} =Value) ->  Value;
+rec(_)-> undefined.
