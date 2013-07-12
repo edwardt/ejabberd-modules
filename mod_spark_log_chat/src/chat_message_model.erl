fb4b1d7123eb7a0c91b479b99c0c9edd7d9ec123
ed <dontemailme@dontemailme.com>
2013-07-12 15:12:58 -0700

    just try putting record in model

--- mod_spark_log_chat/src/chat_message_model.erl
+++ mod_spark_log_chat/src/chat_message_model.erl
@@ -1,9 +1,26 @@
 -module(chat_message_model).
--include_lib("chat_message.hrl").
+%-include_lib("chat_message.hrl").
 
 -behaviour(json_rec_model).
 -compile([parse_transform, exprecs]).
 -export([new/1]).
+
+-record(chat_message, {
+		from = "" ::string(),
+		from_brandId = "" ::string(),
+		to = "" ::string(),
+		to_brandId = "" ::string(),
+		type = "chat" ::string(), 
+		format = "text" ::string(),
+		subject = "" ::string(), 
+		body = "" ::string(), 
+		thread = "" ::string(),
+		time_stamp = "" ::string()}).
+
+
+-export_records([chat_message]).
+
+
 new(<<"chat_message">>)->
    '#new-chat_message'();
 new(_)-> undefined.
