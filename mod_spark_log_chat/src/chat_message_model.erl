122bad309ce58e000f89649b7c6ce420a4138854
ed <dontemailme@dontemailme.com>
2013-07-15 16:19:33 -0700

    compile error

--- mod_spark_log_chat/src/chat_message_model.erl
+++ mod_spark_log_chat/src/chat_message_model.erl
@@ -6,7 +6,7 @@
 -export([new/1, 
 	 	 rec/1,
 	  	 ensure_binary/1]).
--spec new(bitstring())-> chat_message{} | undefined.
+-spec new(bitstring())-> chat_message() | undefined.
 new(<<"chat_message">>)->
    '#new-chat_message'();
 new(_)-> undefined.
@@ -14,9 +14,8 @@ new(_)-> undefined.
 rec(#chat_message{} =Value) ->  Value;
 rec(_)-> undefined.
 
--spec ensure_binary(atom | any()) -> binary().
+-spec ensure_binary(atom() | any()) -> binary().
 ensure_binary(#chat_message{} = Value) ->
 	Json = json_rec:to_json(Value, chat_message_model),
-  ?INFO_MSG("Chat Message as Json ~p",[Json]),
-  lists:flatten(mochijson2:encode(Json));
+    lists:flatten(mochijson2:encode(Json));
 ensure_binary(Val) -> app_util:ensure_binary(Val).
