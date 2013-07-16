393274280f60d885e147fb10257bf1ea2c21f2e6
root <root@chat.dev0.spark.net>
2013-07-16 12:43:51 -0400

    adding until files

---
+++ presence_app/src/app_config_util.erl
@@ -0,0 +1,36 @@
+-module(app_config_util).
+
+-export([
+		cwd/0,
+		config_val/3,
+		load_config/0,
+		load_config/1,
+		load_config/2]).
+
+-define(CONFPATH,"conf").
+
+-spec load_config()-> list().
+load_config()->
+  {ok, ConfDir}= cwd(),
+  load_config(ConfDir, "spark_consumer.config").
+
+-spec load_config(string())-> list().
+load_config(File) ->
+  {ok, ConfDir}= cwd(),
+  load_config(ConfDir,File).
+
+-spec load_config(string(), string())-> list().
+load_config(ConfDir,File) when is_list(ConfDir), 
+			  is_list(File)->
+  FileFullPath = lists:concat([ConfDir,"/", File]),
+  error_logger:info_msg("Loading config: ~p",[FileFullPath]),
+  {ok, [ConfList]}= file:consult(FileFullPath),
+  {ok, [ConfList]}.
+
+-spec cwd()-> {ok, string()}.
+cwd()->
+  {ok, Cwd} = file:get_cwd(),
+  {ok, lists:concat([Cwd,"/",?CONFPATH])}.
+
+-spec config_val(atom(), list(), any()) -> any().
+config_val(Key, Params, Default) -> proplists:get_value(Key, Params, Default).
