0bace1c62ca70b2fb82157bffe0963025f8d1188
ed <dontemailme@dontemailme.com>
2013-07-17 15:53:24 -0700

    addding log tracing

--- presence_app/src/user_presence_sup.erl
+++ presence_app/src/user_presence_sup.erl
@@ -21,6 +21,7 @@
 start_link()->
 	start_link([{?ConfPath, ?ConfFile}]).
 start_link(Args) ->
+    error_logger:info_msg("Starting ~p supervisor with args ~p", lists:flatten(?MODULE, Args)),
     supervisor:start_link({local, ?SERVER}, ?MODULE, Args).
 
 %% ===================================================================
