8d10b4213ec731e9542bd8481524c827178fe224
root <root@chat.dev0.spark.net>
2013-07-20 16:37:16 -0400

    get rid of redunant code

--- presence_app/src/user_presence_api.erl
+++ presence_app/src/user_presence_api.erl
@@ -10,31 +10,31 @@
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the app for inclusion in a supervisor tree
 start_link() ->
-    application:ensure_started(inets),
-    application:ensure_started(crypto),
-    application:ensure_started(mochiweb),
+    app_util:start_app(inets),
+    app_util:start_app(crypto),
+    app_util:start_app(mochiweb),
     application:set_env(webmachine, webmachine_logger_module, 
                         webmachine_logger),
-    ensure_started(webmachine),
+    app_util:start_app(webmachine),
     user_presence_api_sup:start_link().
 
 %% @spec start() -> ok
 %% @doc Start the user_presence_api server.
 start() ->
-    application:ensure_started(inets),
-    application:ensure_started(crypto),
-    application:ensure_started(mochiweb),
+    app_util:start_app(inets),
+    app_util:start_app(crypto),
+    app_util:start_app(mochiweb),
     application:set_env(webmachine, webmachine_logger_module, 
                         webmachine_logger),
-    ensure_started(webmachine),
+    app_util:start_app(webmachine),
     application:start(user_presence_api).
 
 %% @spec stop() -> ok
 %% @doc Stop the user_presence_api server.
 stop() ->
-    Res = application:stop(user_presence_api),
-    application:stop(webmachine),
-    application:stop(mochiweb),
-    application:stop(crypto),
-    application:stop(inets),
+    Res = app_util:stop_app(user_presence_api),
+    app_util:stop_app(webmachine),
+    app_util:stop_app(mochiweb),
+    app_util:stop_app(crypto),
+    app_util:stop_app(inets),
     Res.
